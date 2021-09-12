e67f2900b191551df4b56e038fe89f1d84231788
Charlie Humphrey <charliehumphrey@Charlies-MacBook-Pro.local>
2021-09-11 18:16:56 -0600

    added css and views

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express')
-const app = express();
+const methodOverride  = require('method-override');
 const redis = require('redis')
 const client = redis.createClient();
+
+const port = 3000;
+const app = express();
+
+app.use(express.static('public'));
+//MiddleWa
+app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
+app.use('/public', express.static('public'));
 
 client.on('connect', function() {
     console.log('Connected!');
   });
 
 app.get('/', ( req, res )=>{
-    res.send('Hello World!');
+    res.render('views/show');
   });
 
-  app.listen(3000);
+
+  // NEW //
+app.get('/new', (req, res) => {
+  res.render('new.ejs');
+  });
+
+  
+// =======================================
+//              LISTENER
+// =======================================
+  app.listen(port);
